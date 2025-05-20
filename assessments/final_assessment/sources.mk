25b50be073f853ef3747c782b952b425cb53fdba
Young.Chang <young.chang.company@gmail.com>
2025-05-20 20:15:18 +0800

    modify sources.mk

--- assessments/final_assessment/sources.mk
+++ assessments/final_assessment/sources.mk
@@ -12,15 +12,20 @@
 # Add your Source files to this variable
 
 ifeq ($(PLATFORM),MSP432)
-	SOURCES =          main.c \
-			   memory.c \
-			   interrupts_msp432p401r_gcc.c \
-			   startup_msp432p401r_gcc.c \
-			   system_msp432p401r.c
+	SOURCES =	src/main.c \
+          		src/memory.c \
+          		src/data.c \
+          		src/course1.c \
+          		src/stats.c \
+          		src/interrupts_msp432p401r_gcc.c \
+          		src/startup_msp432p401r_gcc.c \
+          		src/system_msp432p401r.c
 else
-	SOURCES = main.c \
-		  memory.c 
-
+	SOURCES =	src/main.c \
+          		src/memory.c \
+          		src/course1.c \
+          		src/data.c \
+          		src/stats.c
 	endif
 
 # Add your include paths to this variable

161e8d36e711669fb4fe10ccdc582f6a9c0d96d6
Young.Chang <young.chang.company@gmail.com>
2025-05-21 19:17:36 +0800

    modify main.c

--- assessments/final_assessment/src/main.c
+++ assessments/final_assessment/src/main.c
@@ -21,9 +21,18 @@
  *
  */
 
+// #include "course1.h"
+
+// #ifdef COURSE1
+//   course1();
+// #endif
+
 #include "course1.h"
 
+int main(void) {
 #ifdef COURSE1
-  course1();
+    course1();
 #endif
+    return 0;
+}
 
